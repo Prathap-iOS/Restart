15dfeadc018169da5e05c5a2c55d6c37f50a64e3
NowFloats <prathap.reddy@nowfloats.com>
2023-09-15 19:12:44 +0530

    Restart starter project

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Prathap Reddy on 15/09/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
